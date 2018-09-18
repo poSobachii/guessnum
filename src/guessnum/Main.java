f40276c411e660966a025031b613656727db199a
Alex <grishanov@inbox.lv>
2018-09-18 20:50:41 +0300

    5 java lesson

--- src/guessnum/Main.java
+++ src/guessnum/Main.java
@@ -1,5 +1,8 @@
 package guessnum;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Main {
@@ -8,16 +11,18 @@ public class Main {
     static List<GameResult> results = new ArrayList<>();
 
     public static void main(String[] args) {
+        loadResults();
+
         String answer;
 
         do {
             System.out.println("Enter your name ?");
             String name = scan.next();
-            long t1 = System.currentTimeMillis();
+
 
             int myNum = rand.nextInt(100) + 1;
 
-
+            long t1 = System.currentTimeMillis();
             boolean userLost = true;
             for (int i = 1; i <= 10; i++) {
                 System.out.println("Try #" + i + " Choose a number between 0 and 100");
@@ -29,21 +34,21 @@ public class Main {
                     System.out.println("higher");
                 } else {
                     long t2 = System.currentTimeMillis();
-                    double time;
-                    time = (t2 - t1) / 1000;
+                    long time = (t2 - t1);
                     System.out.println("correct - YOU WON");
-                    System.out.println("Your time is " + time + " seconds");
+                    System.out.println("Your time is " + time / 1000.0 + " seconds");
                     userLost = false;
                     GameResult r = new GameResult();
                     r.name = name;
                     r.triesCount = i;
                     r.time = time;
                     results.add(r);
+                    results.sort(Comparator.<GameResult>comparingInt(x -> x.triesCount).thenComparingLong(x -> x.time));
 
                     break;
                 }
             }
-            if (userLost == true) {  // mozno prosto (userlost) - avtomaticheski schitaet estj li tam true ili !userLost - not true ?
+            if (userLost == true) {     // mozno prosto (userlost) - avtomaticheski schitaet estj li tam true ili !userLost - not true ?
                 System.out.println("YOU LOST !");
             }
 
@@ -53,63 +58,99 @@ public class Main {
 
 
         showResults();
+        saveResults();
         System.out.println("Goodbye !");
     }
 
-    private static void showResults() {
-        for (GameResult r : results) {
-            System.out.println(r.name + ". Your tries --> " + r.triesCount + ". Your time --> " + r.time + " seconds !");
+    private static void loadResults() {
+        File plum = new File("top_scores.txt");
+        try (Scanner shmeks = new Scanner(plum)) {
+
+            while (shmeks.hasNext()) {
+                GameResult apple = new GameResult();
+                apple.name = shmeks.next();
+                apple.triesCount = shmeks.nextInt();
+                apple.time = shmeks.nextLong();
+                results.add(apple);
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot load from a file");
         }
     }
 
-    static String askYN() {
-        String answer;
-        do {
-            answer = scan.next();
-            if (!answer.equals("Y") && !answer.equals("N")) {
-                System.out.println("Type Y or N only");
-                continue;   // continue - delaj sledujuwu interaciju cikla - fori , do/while lubogo cikla snachala
-            } else {
-                break;
+    private static void saveResults() {
+        File plum = new File("top_scores.txt");
+        try (PrintWriter xeks = new PrintWriter(plum)) {
+            for (GameResult r : results) {
+                xeks.printf("%s %d %d\n", r.name, r.triesCount, r.time);
             }
-        } while (true);
-        return answer;
-
+        } catch (IOException e) {
+            System.out.println("Cannot save to file");
+        }
     }
 
-//    static int askNum() {
-//        int userNum;
-//        do {
-//            userNum = scan.nextInt();
-//            if ( userNum < 0) {
-//                System.out.println("Type only in 0 - 100 range");
-//                continue;
-//            } else if ( userNum > 100) {
-//                System.out.println("Type only in 0 - 100 range");
-//                continue;
-//            } else {
+//    private static void showResults() {
+//        int count = 0;
+//        for (GameResult r : results) {
+//            System.out.println(r.name + ". Your tries --> " + r.triesCount + ". Your time --> " + r.time / 1000 + " seconds !");
+//            count++;
+//        if (count == 3){
 //                break;
 //            }
-//        } while (true);
-//        return userNum;
+//        }
+
+//    private static void showResults() {
+//        for (GameResult r : results) {
+//            System.out.println(r.name + ". Your tries --> " + r.triesCount + ". Your time --> " + r.time / 1000 + " seconds !");
+//        }
 //    }
 
+//    private static void showResults() {
+//       int index = Math.min(5, results.size());
+//        for (int i = 0; i < index; i++) {
+//            GameResult r = results.get(i);
+//                System.out.println(r.name + ". Your tries --> " + r.triesCount + ". Your time --> " + r.time / 1000 + " seconds !");
+//        }
 
-    static int askNum() {
-        int answer;
-        do {
-            try {
-                answer = scan.nextInt();
-            } catch (InputMismatchException k) {
-                System.out.println("This isn't a number");
-                scan.next();
-                continue;
-            }
-            if (answer < 0 || answer > 100) {
-                System.out.println("Please enter in 0 - 100 range");
-            } else {
-                return answer;
-            }
-        } while (true);
+    private static void showResults() {
+       results.stream()
+               .limit(5)
+               .forEach( r -> {
+                   System.out.println(r.name + ". --> " + r.triesCount + ". --> " + r.time / 1000 + " secs !");
+               });
     }
-}
+
+
+        static String askYN () {
+            String answer;
+            do {
+                answer = scan.next();
+                if (!answer.equals("Y") && !answer.equals("N")) {
+                    System.out.println("Type Y or N only");
+                    continue;   // continue - delaj sledujuwu interaciju cikla - fori , do/while lubogo cikla snachala
+                } else {
+                    break;
+                }
+            } while (true);
+            return answer;
+
+        }
+
+        static int askNum () {
+            int answer;
+            do {
+                try {
+                    answer = scan.nextInt();
+                } catch (InputMismatchException k) {
+                    System.out.println("This isn't a number");
+                    scan.next();
+                    continue;
+                }
+                if (answer < 0 || answer > 100) {
+                    System.out.println("Please enter in 0 - 100 range");
+                } else {
+                    return answer;
+                }
+            } while (true);
+        }
+    }
